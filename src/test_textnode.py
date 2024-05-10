bf361f2feed96535d0dae19fd106ff193e8a2bb2
Eduardo Diaz <eduardado@gmail.com>
2024-05-10 16:10:51 +1000

    added more test functions to TextNode class

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,22 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_link_with_url(self):
+        node = TextNode("Responsive web design portfolio", TextType.LINK, "https://eduardado.github.io/")
+        self.assertEqual(node.__repr__(), "TextNode(Responsive web design portfolio, link, https://eduardado.github.io/)")
+
+    def test_link_type_without_url(self):
+        node = TextNode("Responsive web design portfolio", TextType.LINK)
+        self.assertEqual(node.text, "Responsive web design portfolio")
+        self.assertEqual(node.text_type, TextType.LINK)
+        self.assertIsNone(node.url)
+
+    def test_text_type_different(self):
+        node = TextNode("Responsive web design portfolio", TextType.LINK)
+        node2 = TextNode("Responsive web design portfolio", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+
 
 if __name__ == "__main__":
     unittest.main()
