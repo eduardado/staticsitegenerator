92df678f54a341ba154d13c57572b1aa56939a8c
Eduardo Diaz <eduardado@gmail.com>
2024-05-19 14:21:58 +1000

    extracting images and links

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, text_type_text, text_type_bold, text_type_italic, text_type_code
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -19,3 +20,14 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+def extract_markdown_images(text):
+    pattern = r"!\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+def extract_markdown_links(text):
+    pattern = r"\[(.*?)\]\((.*?)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,7 @@
 import unittest
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, text_type_text, text_type_bold, text_type_italic, text_type_code
+import re
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_delimiter_bold(self):
@@ -43,7 +44,7 @@ class TestInlineMarkdown(unittest.TestCase):
             new_nodes
         )
 
-    def test_delimiter_bold_double_multiword(self):
+    def test_delimiter_bold_double_multiword_italic(self):
         node = TextNode("I *really love* ramen. *Especially if* I contribute on the making.", text_type_text)
         new_nodes = split_nodes_delimiter([node], "*", text_type_italic)
         self.assertListEqual(
@@ -56,3 +57,11 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes
         )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images("This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)")
+        self.assertListEqual(matches, [("image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"), ("another", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png")])
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links("This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)")
+        self.assertListEqual(matches, [("link", "https://www.example.com"), ("another", "https://www.example.com/another")])
