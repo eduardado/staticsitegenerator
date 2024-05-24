1a5bc5d32c6de43578ffdc1bbb4073b7cc242f3e
Eduardo Diaz <eduardado@gmail.com>
2024-05-24 17:00:22 +1000

    extra test

--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -95,3 +95,17 @@ class TestBlockMarkdown(unittest.TestCase):
         block = "1. One\n2 Two"
         block_type = block_to_block_type(block)
         self.assertNotEqual(block_type, block_type_olist)
+
+        def test_block_to_block_type_extra(self):
+            block = "# heading"
+            self.assertEqual(block_to_block_type(block), block_type_heading)
+            block = "```\ncode\n```"
+            self.assertEqual(block_to_block_type(block), block_type_code)
+            block = "> quote\n> more quote"
+            self.assertEqual(block_to_block_type(block), block_type_quote)
+            block = "* list\n* items"
+            self.assertEqual(block_to_block_type(block), block_type_ulist)
+            block = "1. list\n2. items"
+            self.assertEqual(block_to_block_type(block), block_type_olist)
+            block = "paragraph"
+            self.assertEqual(block_to_block_type(block), block_type_paragraph)
