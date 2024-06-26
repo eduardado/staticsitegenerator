4005e3b87bf81fc3d21b83f5cfff471ff0a547d7
Eduardo Diaz <eduardado@gmail.com>
2024-06-26 15:48:19 +1000

    copy static

--- src/main.py
+++ src/main.py
@@ -1,13 +1,7 @@
 from textnode import TextNode
-from texttype import TextType
+from copy_static import copy_static_to_public
 
 def main():
-    textNode = TextNode("This is a text node", TextType.BOLD, "http://www.boot.dev")
-    textNode2 = TextNode("This is another text node", TextType.CODE)
-    print(textNode.__repr__())
-    print(textNode2.__repr__())
+    copy_static_to_public()
 
-    print("__eq__() method test:")
-    textNode3 = TextNode("This is another text node", TextType.CODE)
-    print(f"textNode2 is equal to textNode3: {textNode3.__eq__(textNode2)}")
 main()
