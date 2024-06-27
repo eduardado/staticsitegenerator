7e2769226ac64a45f7beb1419067e0350af0042e
Eduardo Diaz <eduardado@gmail.com>
2024-06-27 16:23:23 +1000

    refactor

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 from textnode import TextNode
-from copy_static import copy_static_to_public
+from copystatic import copy_files_recursively
+import os
+import shutil
+
+# variables to store the relative path of static and public directories from the main directory of the project
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-    copy_static_to_public()
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursively(dir_path_static, dir_path_public)
 
 main()
